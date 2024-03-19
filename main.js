d18664b02e0afbb565bf9cfc1ae7f78e1cb7e797
hooriya <parih3080@gmail.com>
2024-03-19 23:12:40 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,29 @@
+// BMI CALCULATOR USING INQUIRER PROMPT;
+import inquirer from "inquirer";
+const input1 = await inquirer.prompt([{
+        name: 'weight',
+        type: 'number',
+        message: 'Please enter your weight in kg:',
+    }]);
+const input2 = await inquirer.prompt([{
+        name: 'height',
+        type: 'number',
+        message: 'Please enter your height in meter:',
+    }]);
+const weight = input1.weight;
+const height = input2.height;
+const bmiCalculator = weight / (height * height);
+console.log(bmiCalculator);
+// CONDITIONAL STATMENT:
+if (bmiCalculator < 18.5) {
+    console.log('you are underweight range!');
+}
+else if (bmiCalculator < 24.9) {
+    console.log('you are healthy weight range!');
+}
+else if (bmiCalculator < 29.9) {
+    console.log('you are  overweight range!');
+}
+else {
+    console.log('your are obese range!');
+}
